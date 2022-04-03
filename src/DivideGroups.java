93b1f7999e67f7323b451c537ea0762cf5fb8d5e
HYKTSNG <Sin5hayak@gmail.com>
2022-04-03 15:41:36 +0900

    １回目のsccan が無視されてしまうバグ発生中

--- src/DivideGroups.java
+++ src/DivideGroups.java
@@ -8,12 +8,23 @@ public class DivideGroups {
 
         Scanner scanner = new Scanner(System.in);
 
-        List<String> originalNamesList = new ArrayList<>(
-                Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"));
+        // List<String> originalNamesList = new ArrayList<>(
+        // Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+        // "M", "N"));
+        List<String> originalNamesList = new ArrayList<>();
+
+        System.out.println("何人のグループですか？");
+        Integer originalMembers = scanner.nextInt();
+
+        for (int i = 1; i <= originalMembers; i++) {
+            System.out.println("メンバーの名前を入力してください。");
+            System.out.println(i + "人目");
+            // １回目のsccan が無視されてしまうバグ
+            String m = scanner.nextLine();
+            originalNamesList.add(m);
+        }
 
         String[] shuffleList = ShuffleList.shuffleList(originalNamesList);
-
-        // System.out.println(Arrays.toString(shuffleList));
 
         System.out.println("何人ずつに分けますか？");
         Integer listCntsystemIn = scanner.nextInt();
